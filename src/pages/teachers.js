d34abb2e13c4fe457a6eec50f4fa5c69d056c89c
Lam Bui Bao <lambuibao@outlook.com>
2022-04-04 21:20:40 +0700

    file rename and indentation issues

--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import Seo from '../components/seo'
 
+// import images
+import bgImage from '../images/background.jpg'
+import img1 from '../images/teachers/1.png'
+import img2 from '../images/teachers/2.png'
+import img3 from '../images/teachers/3.png'
+import img4 from '../images/teachers/4.png'
+import img5 from '../images/teachers/5.png'
+import img6 from '../images/teachers/6.png'
+import img7 from '../images/teachers/7.png'
+import img8 from '../images/teachers/8.png'
+import img9 from '../images/teachers/9.png'
+import img10 from '../images/teachers/10.png'
+
 export default function Teachers() {
   return (
     <>
@@ -10,6 +23,27 @@ export default function Teachers() {
 
     <div className="navbar">
       <Navbar />
+    </div>
+
+    <div className="background">
+      <div className="background-image">
+        <style>
+          {
+            `.background-image {
+              background-image: url(${bgImage});
+              background-size: cover;
+              background-position: center;
+              background-repeat: no-repeat;
+              width: 100%;
+              height: 100%;
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: -1;
+            }`
+          }
+        </style>
+      </div>
     </div>
 
     <div className="header-text">
@@ -26,26 +60,41 @@ export default function Teachers() {
         letterSpacing: '0.1em',
         textShadow: '-2px -2px 5px #000',
       }}>
-        Teachers</h1>
+        Teachers
+      </h1>
     </div>
 
     <div className="image-grid">
       <div className="image-grid-item">
-        <img src={} alt="teacher" />
+        <img src={ img1 } alt="teacher" />
       </div>
       <div className="image-grid-item">
-        <img src={} alt="teacher" />
+        <img src={ img2 } alt="teacher" />
       </div>
       <div className="image-grid-item">
-        <img src={} alt="teacher" />
+        <img src={ img3 } alt="teacher" />
       </div>
       <div className="image-grid-item">
-        <img src={} alt="teacher" />
+        <img src={ img4 } alt="teacher" />
       </div>
       <div className="image-grid-item">
-        <img src={} alt="teacher" />
+        <img src={ img5 } alt="teacher" />
       </div>
-
+      <div className="image-grid-item">
+        <img src={ img6 } alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={ img7 } alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={ img8 } alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={ img9 } alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={ img10 } alt="teacher" />
+      </div>
     </div>
     </>
   )
