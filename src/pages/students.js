e39410191e7f40751c2bca844c48abdc51b04c7b
Lam Bui Bao <lambuibao@outlook.com>
2022-04-04 11:52:11 +0700

    imported files

--- src/pages/students.js
+++ src/pages/students.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import Seo from '../components/seo'
 
+// import images
+import img1 from '../images/students/1.png'
+import img2 from '../images/students/2.png'
+import img3 from '../images/students/3.png'
+import img4 from '../images/students/4.png'
+import img5 from '../images/students/5.png'
+import img6 from '../images/students/6.png'
+import img7 from '../images/students/7.png'
+import img8 from '../images/students/8.png'
+import img9 from '../images/students/9.png'
+import img10 from '../images/students/10.png'
+import img11 from '../images/students/11.png'
+import img12 from '../images/students/12.png'
+import img13 from '../images/students/13.png'
+import img14 from '../images/students/14.png'
+import img15 from '../images/students/15.png'
+import img16 from '../images/students/16.png'
+import img17 from '../images/students/17.png'
+import img18 from '../images/students/18.png'
+import img19 from '../images/students/19.png'
+import img20 from '../images/students/20.png'
+import img21 from '../images/students/21.png'
+import img22 from '../images/students/22.png'
+import img23 from '../images/students/23.png'
+
 export default function Students() {
   return (
     <><>
@@ -10,73 +35,73 @@ export default function Students() {
     </><>
         <div className='image-grid'>
           <div className='image-grid-item'>
-            <img src='../images/students/1.png' />
+            <img src={ img1 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/2.png' />
+            <img src={ img2 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/3.png' />
+            <img src={ img3 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/4.png' />
+            <img src={ img4 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/5.png' />
+            <img src={ img5 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/6.png' />
+            <img src={ img6 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/7.png' />
+            <img src={ img7 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/8.png' />
+            <img src={ img8 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/9.png' />
+            <img src={ img9 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/10.png' />
+            <img src={ img10 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/11.png' />
+            <img src={ img11 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/12.png' />
+            <img src={ img12 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/13.png' />
+            <img src={ img13 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/14.png' />
+            <img src={ img14 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/15.png' />
+            <img src={ img15 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/16.png' />
+            <img src={ img16 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/17.png' />
+            <img src={ img17 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/18.png' />
+            <img src={ img18 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/19.png' />
+            <img src={ img19 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/20.png' />
+            <img src={ img20 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/21.png' />
+            <img src={ img21 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/22.png' />
+            <img src={ img22 } />
           </div>
           <div className='image-grid-item'>
-            <img src='../images/students/23.png' />
+            <img src={ img23 } />
           </div>
         </div>
 
--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -7,6 +7,46 @@ export default function Teachers() {
     <div className="seo-title">
         <Seo title="Teachers" />
     </div>
+
+    <div className="navbar">
+      <Navbar />
+    </div>
+
+    <div className="header-text">
+      <h1 style={{
+        fontFamily: 'Montserrat, sans-serif',
+        fontSize: '24px',
+        fontWeight: '500',
+        lineHeight: '1.5',
+        color: 'coral',
+        margin: '0',
+        padding: '0',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        letterSpacing: '0.1em',
+        textShadow: '-2px -2px 5px #000',
+      }}>
+        Teachers</h1>
+    </div>
+
+    <div className="image-grid">
+      <div className="image-grid-item">
+        <img src={} alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={} alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={} alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={} alt="teacher" />
+      </div>
+      <div className="image-grid-item">
+        <img src={} alt="teacher" />
+      </div>
+
+    </div>
     </>
   )
 }
